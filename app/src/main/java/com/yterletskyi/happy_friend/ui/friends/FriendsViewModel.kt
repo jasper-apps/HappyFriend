af7aae159dd815764946981a5fc471160811f2b7
yterletskyi <yterletskyI@gmail.com>
2021-04-09 23:13:24 +0300

    Put under source control

---
+++ app/src/main/java/com/yterletskyi/happy_friend/ui/friends/FriendsViewModel.kt
@@ -0,0 +1,13 @@
+package com.yterletskyi.happy_friend.ui.friends
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FriendsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
