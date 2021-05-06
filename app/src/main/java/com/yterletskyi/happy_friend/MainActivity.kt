654daea8e6467a0a0f4f8ae8deaaa5507688b669
yterletskyi <yterletskyI@gmail.com>
2021-05-06 19:59:24 +0300

    Cut off bottom navigation!

--- app/src/main/java/com/yterletskyi/happy_friend/MainActivity.kt
+++ app/src/main/java/com/yterletskyi/happy_friend/MainActivity.kt
@@ -1,35 +1,7 @@
 package com.yterletskyi.happy_friend
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-                as NavHostFragment
-        val navController = navHost.navController
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_friends,
-                R.id.navigation_calendar,
-                R.id.navigation_settings
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
