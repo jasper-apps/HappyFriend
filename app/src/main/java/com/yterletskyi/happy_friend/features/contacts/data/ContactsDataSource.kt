3951409d3b3d8efe4be9615d53e83c2f5cd609ad
Yurii Terletskyi <Yurii.Terletskyi@tomtom.com>
2022-06-24 22:00:23 +0300

    Optimize searching: full search on start only

--- app/src/main/java/com/yterletskyi/happy_friend/features/contacts/data/ContactsDataSource.kt
+++ app/src/main/java/com/yterletskyi/happy_friend/features/contacts/data/ContactsDataSource.kt
@@ -3,7 +3,6 @@ package com.yterletskyi.happy_friend.features.contacts.data
 
 import android.content.Context
 import android.database.Cursor
-import android.net.Uri
 import android.provider.ContactsContract
 import android.util.Log
 import androidx.core.net.toUri
@@ -63,57 +62,39 @@ class PhoneContactsDataSource @Inject constructor(
     private val birthdayParser: BirthdayParser,
 ) : ContactsDataSource {
 
-    private val _contactsFlow: MutableStateFlow<List<Contact>> = MutableStateFlow(emptyList())
+    private val fullContactList: List<Contact> = queryContacts().orEmpty()
+
+    private val _contactsFlow: MutableStateFlow<List<Contact>> = MutableStateFlow(fullContactList)
     override val contactsFlow: Flow<List<Contact>> = _contactsFlow
 
-    init {
-        search("")
+    override fun search(query: String) {
+        _contactsFlow.value = fullContactList.filter {
+            it.name.contains(query, ignoreCase = true)
+        }
     }
 
-    override fun search(query: String) {
-        val uri = when {
-            query.isBlank() -> ContactsContract.Contacts.CONTENT_URI
-            else -> Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, query)
-        }
-        val list = mutableListOf<Contact>()
-        context.contentResolver.query(
-            uri,
+    private fun queryContacts(): List<Contact>? {
+        return context.contentResolver.query(
+            ContactsContract.Data.CONTENT_URI,
             arrayOf(
-                ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME,
-                ContactsContract.Contacts.PHOTO_THUMBNAIL_URI,
+                ContactsContract.Data.CONTACT_ID,
+                ContactsContract.Data.DISPLAY_NAME,
+                ContactsContract.Data.PHOTO_THUMBNAIL_URI,
+                ContactsContract.CommonDataKinds.Event.START_DATE,
             ),
             "display_name is not null",
             null,
             "display_name"
         )?.use { cursor ->
-            while (cursor.moveToNext()) {
-
-                var contact = cursorToContact(cursor)
-
-                context.contentResolver.query(
-                    ContactsContract.Data.CONTENT_URI,
-                    arrayOf(ContactsContract.CommonDataKinds.Event.START_DATE),
-                    "${ContactsContract.Data.CONTACT_ID}=? AND ${ContactsContract.Data.MIMETYPE}=? AND ${ContactsContract.CommonDataKinds.Event.TYPE}=${ContactsContract.CommonDataKinds.Event.TYPE_BIRTHDAY}",
-                    arrayOf(
-                        contact.id.toString(),
-                        ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE
-                    ),
-                    null,
-                )?.use {
-                    Log.i("info23", "fetching birthday for ${contact.name}")
-                    while (it.moveToNext()) {
-                        val birthdayStr = it.getString(0)
-                        Log.i("info24", "${contact.name} birthday is: $birthdayStr")
-                        contact = contact.copy(birthday = birthdayParser.parse(birthdayStr))
-                    }
+            generateSequence { if (cursor.moveToNext()) cursor else null }
+                .map(::cursorToContact)
+                .groupBy { it.id }
+                .map { (_, contacts) ->
+                    contacts
+                        .singleOrNull { it.birthday != null }
+                        ?: contacts.first()
                 }
-
-                list.add(contact)
-            }
         }
-
-        _contactsFlow.value = list
     }
 
     private fun cursorToContact(cursor: Cursor): Contact {
@@ -121,8 +102,7 @@ class PhoneContactsDataSource @Inject constructor(
             id = cursor.getLong(0),
             name = cursor.getString(1),
             imageUri = cursor.getString(2)?.toUri(),
-//            birthday = cursor.getString(3)?.let(birthdayParser::parse),
-            birthday = null
+            birthday = cursor.getString(3)?.let(birthdayParser::parse),
         )
     }
 
@@ -133,7 +113,12 @@ class TimeMeasuredContactsDataSource(
     private val impl: ContactsDataSource
 ) : ContactsDataSource by impl {
 
+    init {
+        Log.i("info23", "initializing contacts data source")
+    }
+
     override fun search(query: String) {
+        Log.i("info23", "searching for <$query>")
         val time = measureTime { impl.search(query) }
         Log.i("info23", "getContacts with query=<$query> took <${time.inSeconds}> sec")
     }
