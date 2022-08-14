0e91937f32495d01b8d61ddd2e8707afa244a0a9
yterletskyi <yterletskyi@gmail.com>
2022-08-15 00:01:29 +0300

    Highlight dragged idea

--- app/src/main/java/com/yterletskyi/happyfriend/features/ideas/ui/drag/MoveIdeaTouchHelperCallback.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/features/ideas/ui/drag/MoveIdeaTouchHelperCallback.kt
@@ -1,9 +1,12 @@
 package com.yterletskyi.happyfriend.features.ideas.ui.drag
 
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.ACTION_STATE_DRAG
 import androidx.recyclerview.widget.ItemTouchHelper.DOWN
 import androidx.recyclerview.widget.ItemTouchHelper.UP
 import androidx.recyclerview.widget.RecyclerView
+import com.yterletskyi.happyfriend.common.x.dp
 
 class MoveIdeaTouchHelperCallback(
     private val onIdeaMoved: (from: Int, to: Int) -> Unit,
@@ -29,20 +32,32 @@ class MoveIdeaTouchHelperCallback(
         viewHolder: RecyclerView.ViewHolder,
         target: RecyclerView.ViewHolder
     ): Boolean {
+        // TODO: add constants for view types
         return viewHolder.itemViewType == 1 && target.itemViewType == 1
     }
 
     override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
         super.onSelectedChanged(viewHolder, actionState)
-        // TODO: highlight current item
+
+        if (actionState == ACTION_STATE_DRAG) {
+            viewHolder?.itemView?.let {
+                ViewCompat.setElevation(it, DRAG_HIGHLIGHT_ELEVATION)
+            }
+        }
     }
 
     override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
         super.clearView(recyclerView, viewHolder)
-        // TODO: clear highlighting
+        viewHolder.itemView.let {
+            ViewCompat.setElevation(it, 0f)
+        }
         onDragEnded()
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) =
         throw IllegalStateException("Swipes are not allowed")
+
+    companion object {
+        private val DRAG_HIGHLIGHT_ELEVATION = 8f.dp
+    }
 }
