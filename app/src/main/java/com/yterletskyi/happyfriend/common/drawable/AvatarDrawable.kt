47fa6c6097e8b27856f838fc38f9b0916a582438
yterletskyi <yterletskyi@gmail.com>
2022-08-26 16:27:28 +0300

    Redesign 1

--- app/src/main/java/com/yterletskyi/happyfriend/common/drawable/AvatarDrawable.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/common/drawable/AvatarDrawable.kt
@@ -2,46 +2,39 @@ package com.yterletskyi.happyfriend.common.drawable
 
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.ColorFilter
 import android.graphics.Paint
-import android.graphics.PixelFormat
-import android.graphics.RectF
-import android.graphics.drawable.Drawable
+import android.graphics.drawable.PaintDrawable
 import androidx.annotation.ColorInt
+import com.yterletskyi.happyfriend.common.x.dp
 import com.yterletskyi.happyfriend.common.x.sp
 
+/**
+ * @param cornerRadius - set [Int.MAX_VALUE] to make the drawable round
+ */
 class AvatarDrawable(
     private val text: String,
-    private val isRound: Boolean = true,
-    private val textSize: Float = 26.sp,
-    @ColorInt private val bgColor: Int = Color.parseColor("#397BF3")
-) : Drawable() {
+    private val textSize: Float = 18.sp,
+    @ColorInt private val bgColor: Int = Color.parseColor("#F48FB1"),
+    cornerRadius: Float = 8f.dp,
+) : PaintDrawable(bgColor) {
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.WHITE
+        color = Color.BLACK
         textSize = this@AvatarDrawable.textSize
-    }
-
-    private val bgPaint = Paint().apply {
-        isAntiAlias = true
-        color = bgColor
-        style = Paint.Style.FILL
     }
 
     private val fontHeight by lazy { textPaint.fontMetrics.run { top + bottom }.toInt() }
     private val fontWidth by lazy { textPaint.measureText(text).toInt() }
 
+    init {
+        setCornerRadius(cornerRadius)
+    }
+
     override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+
         val h = bounds.height().toFloat()
         val w = bounds.width().toFloat()
-
-        val rectF = RectF().apply {
-            set(0f, 0f, w, h)
-        }
-
-        val corner = if (isRound) h / 2 else 0f
-
-        canvas.drawRoundRect(rectF, corner, corner, bgPaint)
 
         val x = (w - fontWidth) / 2f
         val y = (h - fontHeight) / 2f
@@ -49,9 +42,11 @@ class AvatarDrawable(
         canvas.drawText(text, x, y, textPaint)
     }
 
-    override fun setAlpha(alpha: Int) {}
+    override fun getIntrinsicWidth(): Int = SIZE
 
-    override fun getOpacity(): Int = PixelFormat.TRANSPARENT
+    override fun getIntrinsicHeight(): Int = SIZE
 
-    override fun setColorFilter(colorFilter: ColorFilter?) {}
+    companion object {
+        private const val SIZE = 100
+    }
 }
