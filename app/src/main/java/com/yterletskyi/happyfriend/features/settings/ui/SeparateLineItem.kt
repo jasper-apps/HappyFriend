bcf669ccde6d269e1627324c3303c4dd354455a8
Pavl0-Gural <pg6369509@gmail.com>
2022-08-28 15:29:25 +0300

    Resolve comments and add start and end paddings for line

--- app/src/main/java/com/yterletskyi/happyfriend/features/settings/ui/SeparateLineItem.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/features/settings/ui/SeparateLineItem.kt
@@ -3,24 +3,29 @@ package com.yterletskyi.happyfriend.features.settings.ui
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import androidx.core.view.forEachIndexed
+import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
 
 class SeparateLineItem : RecyclerView.ItemDecoration() {
 
     private val paint = Paint().apply {
-        color = Color.rgb(211, 211, 211)
+        color = Color.parseColor("#d3d3d3")
     }
 
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        var width = 0f
-        var y = 0f
-        parent.forEachIndexed { index, view ->
-            if (index == parent.childCount - 1) {
-                width = view.width.toFloat()
-                y = view.y
-            }
-        }
-        c.drawLine(0f, y - 8f, width, y - 8f, paint)
+        val width = parent[parent.childCount - 1].width.toFloat()
+        val y = parent[parent.childCount - 1].y
+        c.drawLine(
+            0f + separator_line_width,
+            y - separator_line_height,
+            width - separator_line_width,
+            y - separator_line_height,
+            paint
+        )
+    }
+
+    companion object {
+        const val separator_line_height = 8f
+        const val separator_line_width = 45f
     }
 }
