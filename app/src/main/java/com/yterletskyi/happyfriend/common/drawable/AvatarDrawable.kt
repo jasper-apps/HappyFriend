1c52fb2de5d80ce0aaae2c23acc84831547eb37d
yterletskyi <yterletskyi@gmail.com>
2022-08-28 20:48:15 +0300

    Tweak light theme

--- app/src/main/java/com/yterletskyi/happyfriend/common/drawable/AvatarDrawable.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/common/drawable/AvatarDrawable.kt
@@ -5,21 +5,20 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.PaintDrawable
 import androidx.annotation.ColorInt
-import com.yterletskyi.happyfriend.common.x.dp
 import com.yterletskyi.happyfriend.common.x.sp
 
 /**
- * @param cornerRadius - set [Int.MAX_VALUE] to make the drawable round
+ * @param cornerRadius - set [Float.MAX_VALUE] to make the drawable round
  */
 class AvatarDrawable(
     private val text: String,
     private val textSize: Float = 18.sp,
-    @ColorInt private val bgColor: Int = Color.parseColor("#F48FB1"),
-    cornerRadius: Float = 8f.dp,
+    @ColorInt private val bgColor: Int = Color.parseColor("#FF6200EE"),
+    cornerRadius: Float = Float.MAX_VALUE,
 ) : PaintDrawable(bgColor) {
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.BLACK
+        color = Color.WHITE
         textSize = this@AvatarDrawable.textSize
     }
 
--- app/src/main/java/com/yterletskyi/happyfriend/features/contacts/ui/ContactsAdapterDelegate.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/features/contacts/ui/ContactsAdapterDelegate.kt
@@ -26,8 +26,8 @@ class ContactsAdapterDelegate(
             text.text = item.fullName
             birthday.text = item.birthday
             starBtn.setImageResource(
-                if (item.isFriend) R.drawable.ic_baseline_favorite_24
-                else R.drawable.ic_baseline_favorite_border_24
+                if (item.isFriend) R.drawable.ic_baseline_favorite_24_selected
+                else R.drawable.ic_baseline_favorite_24
             )
         }
     }
