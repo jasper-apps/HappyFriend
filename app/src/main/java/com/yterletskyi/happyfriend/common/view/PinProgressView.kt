0568ca4914d0c97747910334e138b35b7644b818
yterletskyi <yterletskyi@gmail.com>
2022-08-29 22:01:53 +0300

    Add color attrs

--- app/src/main/java/com/yterletskyi/happyfriend/common/view/PinProgressView.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/common/view/PinProgressView.kt
@@ -6,17 +6,60 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.ColorInt
+import androidx.core.content.res.getColorOrThrow
+import com.yterletskyi.happyfriend.R
 import com.yterletskyi.happyfriend.common.x.dp
 
 class PinProgressView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    var step: Int = 0
+    @ColorInt
+    var defaultCircleColor: Int = Color.BLACK
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    @ColorInt
+    var progressCircleColor: Int = Color.BLACK
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var step: Int = 3
     var steps: Int = 4
 
-    private val paint: Paint = Paint().apply {
-        color = Color.RED
+    private val defaultCirclePaint: Paint by lazy {
+        Paint().apply { color = defaultCircleColor }
+    }
+
+    private val progressCirclePaint: Paint by lazy {
+        Paint().apply { color = progressCircleColor }
+    }
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.PinProgressView,
+            0,
+            0
+        ).apply {
+            try {
+                defaultCircleColor = getColorOrThrow(
+                    R.styleable.PinProgressView_defaultCircleColor
+                )
+                progressCircleColor = getColorOrThrow(
+                    R.styleable.PinProgressView_progressCircleColor
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                recycle()
+            }
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -38,6 +81,7 @@ class PinProgressView @JvmOverloads constructor(
         var startX = CIRCLE_DIAMETER / 2f
 
         for (s in 0 until steps) {
+            val paint = if (s < step) progressCirclePaint else defaultCirclePaint
             canvas.drawCircle(startX, y, CIRCLE_DIAMETER / 2f, paint)
             startX += CIRCLE_DIAMETER + BETWEEN_CIRCLE_SPACE
         }
