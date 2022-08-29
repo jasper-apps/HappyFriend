95f46313b2e8474a767df2fa16b9a8ec555ad8ee
Pavl0-Gural <pg6369509@gmail.com>
2022-08-29 15:01:18 +0300

    Add indents for line

--- app/src/main/java/com/yterletskyi/happyfriend/features/settings/ui/SeparateLineItem.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/features/settings/ui/SeparateLineItem.kt
@@ -3,8 +3,11 @@ package com.yterletskyi.happyfriend.features.settings.ui
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
+import android.view.View
 import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
+import com.yterletskyi.happyfriend.common.x.dp
 
 class SeparateLineItem : RecyclerView.ItemDecoration() {
 
@@ -25,8 +28,22 @@ class SeparateLineItem : RecyclerView.ItemDecoration() {
         )
     }
 
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val adapter = parent.adapter ?: return
+
+        when (parent.getChildAdapterPosition(view)) {
+            adapter.itemCount - 2 -> outRect.bottom = 8.dp
+            adapter.itemCount - 1 -> outRect.top = 8.dp
+        }
+    }
+
     companion object {
         private const val SEPARATE_LINE_POSITION = 8f
-        private const val SEPARATE_LINE_INDENT = 45f
+        private var SEPARATE_LINE_INDENT = 8.dp
     }
 }
