0a65db0b4c70cf429904a3f3b89cd1ba139f3a38
Pavl0-Gural <pg6369509@gmail.com>
2022-08-30 23:05:23 +0300

    Create PIN Setup fragment

---
+++ app/src/main/java/com/yterletskyi/happyfriend/features/pin/ui/SetupPinFragment.kt
@@ -0,0 +1,14 @@
+package com.yterletskyi.happyfriend.features.pin.ui
+
+import android.os.Bundle
+import android.view.View
+import com.yterletskyi.happyfriend.common.binding.BaseBindingFragment
+import com.yterletskyi.happyfriend.databinding.FragmentPinsetupBinding
+
+class SetupPinFragment : BaseBindingFragment<FragmentPinsetupBinding>(
+    FragmentPinsetupBinding::inflate
+) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
