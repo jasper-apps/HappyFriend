19d57c051c022decd31af1fa5ad897d952598469
yterletskyi <yterletskyi@gmail.com>
2022-08-30 11:54:58 +0300

    Layout keyboard v1

--- app/src/main/java/com/yterletskyi/happyfriend/common/view/PinKeyboardView.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/common/view/PinKeyboardView.kt
@@ -2,18 +2,19 @@ package com.yterletskyi.happyfriend.common.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.yterletskyi.happyfriend.databinding.ViewPinKeyboardBinding
 
 class PinKeyboardView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : ConstraintLayout(context, attrs) {
 
+    private var _binding: ViewPinKeyboardBinding? = null
+    private val binding: ViewPinKeyboardBinding get() = _binding!!
+
     init {
-    }
-
-    private class PinKeyboardButtonView @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null
-    ) : ConstraintLayout(context, attrs) {
-
+        val inflater = LayoutInflater.from(context)
+        _binding = ViewPinKeyboardBinding.inflate(inflater, this)
     }
 }
