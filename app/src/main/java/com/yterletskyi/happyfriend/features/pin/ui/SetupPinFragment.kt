3dba1e356a6cbe0866c9656569996d6275cc68d6
Pavl0-Gural <pg6369509@gmail.com>
2022-08-31 15:53:57 +0300

    Add navigation to pin screen using next button

--- app/src/main/java/com/yterletskyi/happyfriend/features/pin/ui/SetupPinFragment.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/features/pin/ui/SetupPinFragment.kt
@@ -2,6 +2,8 @@ package com.yterletskyi.happyfriend.features.pin.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.yterletskyi.happyfriend.R
 import com.yterletskyi.happyfriend.common.binding.BaseBindingFragment
 import com.yterletskyi.happyfriend.databinding.FragmentPinsetupBinding
 
@@ -9,6 +11,11 @@ class SetupPinFragment : BaseBindingFragment<FragmentPinsetupBinding>(
     FragmentPinsetupBinding::inflate
 ) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        binding.nextButton.setOnClickListener {
+            val action = SetupPinFragmentDirections.toPinScreen(getString(R.string.title), "")
+            findNavController().navigate(action)
+        }
     }
 }
+
+
