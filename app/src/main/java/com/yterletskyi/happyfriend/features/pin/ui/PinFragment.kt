5510d888a1e7c19c267fc087e89b3b52bd0a4c9d
Pavl0-Gural <pg6369509@gmail.com>
2022-09-12 16:12:52 +0300

    Remove unnecessary code

--- app/src/main/java/com/yterletskyi/happyfriend/features/pin/ui/PinFragment.kt
+++ app/src/main/java/com/yterletskyi/happyfriend/features/pin/ui/PinFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import com.yterletskyi.happyfriend.common.binding.BaseBindingFragment
 import com.yterletskyi.happyfriend.databinding.FragmentPinBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,7 +14,6 @@ class PinFragment : BaseBindingFragment<FragmentPinBinding>(
 ) {
 
     private val viewModel: PinViewModel by viewModels()
-    private val args: PinFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
