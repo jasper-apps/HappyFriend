7b26da7f972ab2d940bff7b8e6d74b18149a786b
yterletskyi <yterletskyi@gmail.com>
2022-09-13 22:35:11 +0300

    Create analytics

---
+++ app/src/main/java/com/yterletskyi/happyfriend/common/analytics/ToLogAnalytics.kt
@@ -0,0 +1,12 @@
+package com.yterletskyi.happyfriend.common.analytics
+
+import com.yterletskyi.happyfriend.common.logger.logcatLogger
+
+class ToLogAnalytics : Analytics {
+
+    private val logger by logcatLogger { "analytics" }
+
+    override fun setUserProperty(property: Analytics.UserProperty<*>) {
+        logger.info("user property set: <${property.key}>: <${property.value.toString()}>")
+    }
+}
