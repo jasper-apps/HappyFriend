28f0b94fc8afffa78656ad8b257bfd800eac60e8
yterletskyi <yterletskyi@gmail.com>
2023-01-16 12:15:41 +0200

    Remove unused enum

--- app/src/main/java/com/jasperapps/happyfriend/features/settings/domain/SettingEnum.kt
+++
@@ -1,8 +0,0 @@
-package com.jasperapps.happyfriend.features.settings.domain
-
-@Deprecated("Use SettingsModelItem")
-enum class SettingEnum {
-    MY_WISHLIST,
-    APP_VERSION,
-    GENERAL_IDEAS,
-}
